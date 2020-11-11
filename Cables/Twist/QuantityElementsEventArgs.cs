ad252057b117d1beb0448bb9fc242a2881b78bba
Алексей Бондаренко <delchew@gmail.com>
2020-11-11 12:26:10 +0300

    Add QuantityElementsChanged Delegate and EventArgs

---
+++ Cables/Twist/QuantityElementsEventArgs.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Cables
+{
+    public delegate void QuantityElementsChangedEventHandler(object sender, QuantityElementsChangedEventArgs e);
+    public class QuantityElementsChangedEventArgs : EventArgs
+    {
+        public TwistInfo TwistInfo { get; }
+        public QuantityElementsChangedEventArgs(TwistInfo twistInfo)
+        {
+            TwistInfo = twistInfo;
+        }
+    }
+}
