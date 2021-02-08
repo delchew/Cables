2f25b057fd658b575fcd19404b70d0543bb812ba
delchew <delchew@mail.ru>
2021-02-08 18:40:28 +0300

    changed Insulated Billet entity

--- Cables/CableElements/InsulatedBillet.cs
+++ Cables/CableElements/InsulatedBillet.cs
@@ -4,16 +4,17 @@ namespace Cables.CableElements
 {
     public class InsulatedBillet : IExtrudable<IConductor>
     {
-        public ExtrusionPolymer ExtrusionPolymer { get; set; }
+        public IConductor Billet { get; set; } //public long ConductorId { get; set; }
 
-        public IConductor Billet { get; set; }
+        public ExtrusionPolymer ExtrusionPolymer { get; set; } //public long PolymerGroupId { get; set; } - поменять!
 
-        public double MinThickness { get; set; }
 
-        public double? NominalThickness { get; set; }
+        public double Diameter { get; set; } //public decimal Diameter { get; set; }
 
-        public double Diameter { get; set; }
+        public double MinThickness { get; set; } //public decimal MinThickness { get; set; }
 
-        public string ShortName { get; set; }
+        public double? NominalThickness { get; set; } //public decimal? NominalThickness { get; set; }
+
+        public string ShortName { get; set; } //public long CableShortNameId { get; set; }
     }
 }
