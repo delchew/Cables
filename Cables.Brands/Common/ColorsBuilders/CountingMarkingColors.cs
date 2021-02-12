fc7a2824cdebd7dd06a8d3060516a232bbc0a725
delchew <delchew@mail.ru>
2021-02-12 18:46:48 +0300

    Added Colors Builders

---
+++ Cables.Brands/Common/ColorsBuilders/CountingMarkingColors.cs
@@ -0,0 +1,19 @@
+﻿using System.Drawing;
+
+namespace Cables.Brands.Common.ColorsBuilders
+{
+    public struct CountingMarkingColors
+    {
+        public Color[] CountingColorsSet { get; set; }
+        public Color[] DirectingColorsSet { get; set; }
+        public Color[] OtherColorsSet { get; set; }
+
+        public CountingMarkingColors(Color[] countingColorSet, Color[] directingColorSet, Color[] otherColorsSet)
+        {
+            CountingColorsSet = countingColorSet;
+            DirectingColorsSet = directingColorSet;
+            OtherColorsSet = otherColorsSet;
+        }
+
+    }
+}
---
+++ Cables.Brands/Common/ColorsBuilders/ICableColorsBuilder.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using System.Drawing;
+
+namespace Cables.Brands.Common.ColorsBuilders
+{
+    public interface ICableColorsBuilder<T> where T : Cable
+    {
+        IList<Color[]> GetCableColorsSet(T cableOrder);
+        IDictionary<Color, int> GetCableColorLengthsSet(T cableOrder);
+    }
+}
